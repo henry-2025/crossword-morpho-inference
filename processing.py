5276cc514c654d09b580dfa2e276b82a1f1211ee
Henry Pick <hpick@hmc.edu>
2022-04-25 15:55:34 -0700

    moving to nltk which is much faster

--- processing.py
+++ processing.py
@@ -1,57 +1,44 @@
+from logging import exception
 import os
 import argparse
-import pickle
-import spacy
+import csv
+import nltk
 import re
-
-nlp = spacy.load("en_core_web_sm")
-
-POS_THRESHOLD = 0.3
 
 def main(args):
     # choose to separate files into individual
-    tagger = nlp.get_pipe("tagger")
-    xdre = re.compile("^[A,D]\d+\.")
-    targre = re.compile("~ (.*)$")
-    cluere = re.compile("^[A,D]\d+\. (.*) ~")
-    if (args.separate):
-        os.makedirs(args.out_dir, exist_ok=True)
-        for root, dirs, files in os.walk(args.in_dir):
-            for fname in files:
-                print(fname)
-                in_file = open(os.path.join(root, fname), 'r')
-                
-                clue_pos_pairs = []
-                for l in in_file.readlines():
-                    if xdre.search(l):
-                        targ = targre.search(l)[1]
-                        clue = cluere.search(l)[1]
+    tagger = nltk.tag.PerceptronTagger()
+    writer = csv.writer(args.out_file)
 
-                        targ = nlp(targ)
-                        # pick the parts of speech guesses with probabilities greater than POS_THRESHOLD
-                        pos_guesses = [i for (i,_) in list(filter(lambda x: x[1] > POS_THRESHOLD, enumerate(tagger.model.predict([targ])[0][0])))]
-                        clue_pos_pairs.append((clue, pos_guesses))
-                out_file_path = os.path.join(args.out_dir, os.path.splitext(fname)[0] + ".pkl")
-                out_file = open(out_file_path, 'wb')
-                pickle.dump(clue_pos_pairs, out_file)   
-                out_file.close()                     
-    
-    # create one large file containing all crossword data
-    else:
-        for _, _, files in os.walk(args.directory):
-            pass
+    i = 0
+    args.in_file.readline()
+    for line in args.in_file.readlines():
+        words = line.split()
+        if len(words) < 4: continue
 
-            
+        # make a bunch of features
+        pubid = words[0]
+        year = words[1]
+        targ = words[2]
+        clue = ' '.join(words[3:])
+        targ_tag = tagger.tag([targ])[0][1]
+        targ_len = len(targ)
 
-    
+        writer.writerow([pubid, year, clue, targ, targ_tag])
+        if i % 10000 == 0 and i:
+            print(f"Processed {i} clue/targ pairs")
+        if args.limit:
+            if i > args.limit: break
+        i += 1
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process a directory of crossword data stored in the xd format and output to a pickle file with ")
-    parser.add_argument('in_dir', help="parent directory for xd files")
-    parser.add_argument('out_dir', help='parent directory for tagged pickle output files')
+    parser.add_argument('in_file', help="parent directory for xd files", type=argparse.FileType('r'))
+    parser.add_argument('out_file', help='parent directory for tagged pickle output files', type=argparse.FileType('w'))
     parser.add_argument('--separate', type=bool, help="separate aggregate data into files corresponding to directories")
+    parser.add_argument('--limit', type=int, default=None, help="number of lines in the input file that should be processed")
 
     args = parser.parse_args()
     main(args)
